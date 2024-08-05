7cfd48a3129c678e082f6526253c6df442dfa2a3
macae craque de bola <128196105+macaecraquedebola10@users.noreply.github.com>
2024-08-05 22:54:13 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const meuNome = "vinicius";
+
+const idade = 17;
+
+const altura = 1.77;
+
+const massa = 70;
+
+const cidadeondemora = "saojosedospinhais"
+
+console.log (meuNome)
+
+console.log (idade);
+
+console.log (altura);
+
+console.log (massa);
+
+console.log (cidadeondemora);
